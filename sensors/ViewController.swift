8f5914f39a3c9c6b6e902df616dbf8e5bc69fb27
Koan-Sin Tan <koansin.tan@gmail.com>
2016-09-18 20:53:52 +0800

    Initial Commit

---
+++ sensors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sensors
+//
+//  Created by Koan-Sin Tan on 9/18/16.
+//  Copyright © 2016 MediaTek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
